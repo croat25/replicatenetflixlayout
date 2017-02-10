86a8993d0d36c594cb37fd9ee5fc56fe3681d110
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-10 11:13:11 -0500

    added search bar styling and started user profile

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Logo from './Logo.js';
 import './App.css';
 
-class App extends Component {
-  render() {
+var App=React.createClass({
+      apikey:'9f12be837c048948b384ec6d3a35d36f',
+      getInitialState: function() {
+    return {searchTerm:"", searchUrl:""};
+  },
+  //handle keys
+  handleKeyUp :function(e){
+    //if enter key is enter and the state search is not empty
+    if (e.key === 'Enter' && this.state.searchTerm !== '') {
+        //searchurl with the search term and apply api key
+      var searchUrl = "search/multi?query=" + this.state.searchTerm + "&api_key=" + this.apiKey;
+      this.setState({searchUrl:searchUrl});
+    }
+  },
+    // handles change in search term
+
+   handleChange : function(e){
+      this.setState({searchTerm : e.target.value});    
+  },render: function() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header className="Header">
+          <Logo />
+          <Navigation />
+          <div id="search" className="Search">
+            <input onKeyUp={this.handleKeyUp} onChange={this.handleChange} type="search" placeholder="Search for a title..." value={this.state.searchTerm}/>
+          </div>
+          <UserProfile />
+          
+        </header>
+        
       </div>
     );
   }
-}
+});
+
+//Profile
+
+var UserProfile = React.createClass({
+  render: function() {
+    return (
+      <div className="UserProfile">
+        <div className="User">
+          <div className="name">Bruno Salapic</div>
+          <div className="image"><img src="https://avatars1.githubusercontent.com/u/8978785?v=3&s=460" alt="profile" /></div>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+var Navigation = React.createClass({
+  render: function() {
+    return (
+      <div id="navigation" className="Navigation">
+        <nav>
+          <ul>
+            <li>Browse</li>
+            <li>My list</li>
+            <li>Top picks</li>
+            <li>Recent</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+});
+
+
+
 
 export default App;
